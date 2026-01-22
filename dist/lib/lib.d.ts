49b2e134a699512445ca6598284a19bd9cd61d83
QANSIGLIERE <qansigliere@gmail.com>
2026-01-22 16:29:41 +0300

    JSTS-33

--- dist/lib/lib.d.ts
+++ dist/lib/lib.d.ts
@@ -1,3 +1,4 @@
 export function createFileFromString(fileName: any, stringValue: any): void;
 export function saveBase64AsImage(pngBase64String: any, pathToFile: any): void;
+export function fileExists(pathToFile: any): boolean;
 //# sourceMappingURL=lib.d.ts.map
