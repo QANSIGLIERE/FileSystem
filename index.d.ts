2919d8e5f0001254ed6d7ec59f2a5da54fb7dfd0
QANSIGLIERE <qansigliere@gmail.com>
2026-03-09 12:56:07 +0400

    JSTS-49

--- index.d.ts
+++ index.d.ts
@@ -2,5 +2,6 @@ import { createFileFromString } from "./dist/lib/lib.js";
 import { saveBase64AsImage } from "./dist/lib/lib.js";
 import { fileExists } from "./dist/lib/lib.js";
 import { deleteFile } from "./dist/lib/lib.js";
-export { createFileFromString, saveBase64AsImage, fileExists, deleteFile };
+import { readFile } from "./dist/lib/lib.js";
+export { createFileFromString, saveBase64AsImage, fileExists, deleteFile, readFile };
 //# sourceMappingURL=index.d.ts.map
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var { createFileFromString, saveBase64AsImage, fileExists, deleteFile } = require('./lib/lib.js');
+var { createFileFromString, saveBase64AsImage, fileExists, deleteFile, readFile } = require('./lib/lib.js');
 
 module.exports.createFileFromString = createFileFromString;
 module.exports.saveBase64AsImage = saveBase64AsImage;
 module.exports.fileExists = fileExists;
 module.exports.deleteFile = deleteFile;
+module.exports.readFile = readFile;
